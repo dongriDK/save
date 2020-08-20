7108d293724975001b3ff54fae24d33a18b173cd
dongriDK <45909972+dongriDK@users.noreply.github.com>
2020-08-20 14:14:13 +0900

    Update atten.py

--- atten.py
+++ atten.py
@@ -12,7 +12,7 @@ prefs = {
     "profile.default_content_setting_values.plugins": 1,
     "profile.content_settings.plugin_whitelist.adobe-flash-player": 1,
     "profile.content_settings.exceptions.plugins.*,*.per_resource.adobe-flash-player": 1,
-    "PluginsAllowedForUrls": "https://unihints.hannam.ac.kr/haksa/main.jsp"
+    "PluginsAllowedForUrls": "https://unihints.hannam.ac.kr/"
 }
 
 options.add_experimental_option("prefs",prefs)
